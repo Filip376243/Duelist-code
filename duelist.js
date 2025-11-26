873775de0687f9d391e065d74528c1badf4d4181
Filip376243 <harmanfilip643@gmail.com>
2025-11-26 06:52:31 +0100

    another file

--- duelist.js
+++ duelist.js
@@ -26,7 +26,7 @@ const healPlus = 'https://cdn.glitch.global/1ea19045-4767-4937-982b-a6d7a7d2c370
 const bucksImage = 'https://cdn.glitch.global/1ea19045-4767-4937-982b-a6d7a7d2c370/bucks.png?v=1725694251369';
 const lightingImage = 'https://cdn.glitch.global/1ea19045-4767-4937-982b-a6d7a7d2c370/lighting.png?v=1725206028211';
 
-let difficulty = 1;
+let difficulty = 4;
 let Level = 3;
 
 //another comment
@@ -40,6 +40,7 @@ class Game {
     update() {
         if (this.run) {
             this.fight.update();
+            //some code
         }
     }
 }
